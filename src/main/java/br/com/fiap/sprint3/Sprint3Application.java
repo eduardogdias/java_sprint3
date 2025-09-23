ccbc17970ddf5bfb722cc70b010836a24e3b5dbf
Eduardo Guilherme Dias <eduardogdias.edu@gmail.com>
2025-09-23 13:32:23 -0300

    Flyway salvando no Oracle

--- src/main/java/br/com/fiap/sprint3/Sprint3Application.java
+++ src/main/java/br/com/fiap/sprint3/Sprint3Application.java
@@ -3,11 +3,15 @@ package br.com.fiap.sprint3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Sprint3Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Sprint3Application.class, args);
+
 	}
+	
+
 
 }
--- src/main/resources/db/migration/V2__insert-data.sql
+++ src/main/resources/db/migration/V2__insert-data.sql
@@ -1,14 +1,14 @@
 -- Inserindo Patio
 INSERT INTO TB_PATIO (nome_patio, endereco_patio) VALUES ('Mottu Space 3', 'Av. Butantan, 552');
-
+ 
 -- Inserindo Zona
 INSERT INTO TB_ZONA (tipo_zona, qtd_vaga_zona, fk_patio) VALUES ('Reparo', 10, 1);
-
+ 
 -- Inserindo Moto
 INSERT INTO TB_MOTO (placa_moto, chassi_moto, marca_moto, modelo_moto) VALUES ('1234567', '12345678901234567', 'Honda', 'Pop');
-
+ 
 -- Inserindo Sensor
-INSERT INTO TB_SENSOR (localizacao_sensor, data_sensor, hora_sensor) VALUES ('Entrada principal', DATE '2025-05-23', TIME '22:30:00');
-
+INSERT INTO TB_SENSOR (localizacao_sensor, data_sensor, hora_sensor)
+VALUES ('Entrada principal', DATE '2025-05-23', TO_DATE('22:30:00', 'HH24:MI:SS'));
 -- Inserindo Historico
 INSERT INTO TB_HISTORICO (posicao_hist, fk_moto, fk_zona, fk_sensor) VALUES (2, 1, 1, 1);
